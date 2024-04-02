435b2e18098f3dae58360edad9e264013a5f6efc
MingyuLian <142514410+MingyuLian@users.noreply.github.com>
2024-04-02 19:43:13 +0800

    new change on 2 April

--- myScript.js
+++ myScript.js
@@ -6,9 +6,44 @@ function toggleSection(sectionId) {
   var arrow = document.getElementById("arrow" + sectionId);
   if (details.classList.contains("hidden")) {
       details.classList.remove("hidden");
-      arrow.textContent = "▲"; // Change arrow direction
+      arrow.textContent = "▲"; 
   } else {
       details.classList.add("hidden");
-      arrow.textContent = "▼"; // Change arrow back
+      arrow.textContent = "▼"; 
   }
 }
+
+// author linkedin alert
+function LinkedInalert() {
+  if (confirm("Go to the LinkedIn Page?")) {
+      window.location.href = "https://www.linkedin.com/in/lianmingyu/"; 
+  }
+}
+
+// author phone alert
+function Phonealert() {
+  alert("please call +61(0) 435 718 939, thank you!");
+}
+
+// author form button click
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('button[type="submit"]').addEventListener('click', function(event) {
+      event.preventDefault(); 
+
+      var email = document.getElementById('email').value;
+      var message = document.getElementById('message').value;
+      if (!email || !message) {
+          alert('Please fill all the empties, thank you');
+          return;
+      }
+      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(email)) {
+          alert('Please input valid email adsress, thakn you');
+          return;
+      }
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+      alert('We have recieved your message, thank you');
+  });
+});
+
